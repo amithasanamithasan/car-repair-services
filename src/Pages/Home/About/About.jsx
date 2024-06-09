9add4b3ba7631542746d6d860e42e4c28f04883a
amithasanamithasan <amit35-1773@diu.edu.bd>
2024-06-09 20:45:31 +0600

    responsive all us

--- src/Pages/Home/About/About.jsx
+++ src/Pages/Home/About/About.jsx
@@ -4,17 +4,21 @@ const About = () => {
     return (
         <div className="hero min-h-screen bg-base-200 ">
         <div className="hero-content flex-col lg:flex-row ">
-            <div className='lg:w-1/2 relative h-40 w-40  rounded-md '>
+            <div className='lg:w-1/2 relative h-30 w-40  rounded-md '>
             <img src={img7} className="max-w-sm rounded-lg shadow-2xl" />
              
           <img  src={img8} className="max-w-sm absolute w-60 h-30 
           right-7 border-8 border-white top-24 rounded-lg shadow-2xl" />
             </div>
       
-          <div className='lg:w-1/2'>
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
-            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-            <button className="btn btn-primary">Get Started</button>
+          <div className='lg:w-1/2  space-y-5 md:space-y-10'>
+        <h1 className='text-2xl font-bold text-orange-700'>About Us</h1>
+            <h1 className="text-4xl font-bold">
+        We are qualified & of experience in this field</h1>
+            <p className="py-6">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour,
+                 or randomised words which donot look even slightly believable..</p>
+            <p>the majority have suffered alteration in some form, by injected humour, or randomised words which donot look even slightly believable.</p>
+            <button className="btn bg-[#ea580c]  ">Get More Info</button>
           </div>
         </div>
       </div>
